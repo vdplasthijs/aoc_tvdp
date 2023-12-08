2899dce172fb25bf5dceadda3729c9118aba4af3
Thijs van der Plas <vdplasthijs@gmail.com>
2023-12-08 10:28:04 +0000

    day8

--- aoc_utils.py
+++ aoc_utils.py
@@ -2,3 +2,8 @@ def read_txt_file_lines(folder='day1', filename='input.txt'):
     with open(filename, 'r') as f:
         return [line.strip() for line in f.readlines()]
     
+def gcd(a, b):
+    """find common divisor of all diffs"""   
+    while b:
+        a, b = b, a % b
+    return a
