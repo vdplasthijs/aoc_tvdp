4a17c87ecd3cc17b3e5fa0f34a1b663d9435a5df
Thijs van der Plas <vdplasthijs@gmail.com>
2023-12-10 12:01:15 +0000

    day10

--- aoc_utils.py
+++ aoc_utils.py
@@ -1,4 +1,4 @@
-def read_txt_file_lines(folder='day1', filename='input.txt'):
+def read_txt_file_lines(filename='input.txt'):
     with open(filename, 'r') as f:
         return [line.strip() for line in f.readlines()]
     
