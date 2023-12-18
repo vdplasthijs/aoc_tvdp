4511ca4c760667fed48238189e75d41bf3437f4a
Thijs van der Plas <vdplasthijs@gmail.com>
2023-12-18 18:40:14 +0000

    day16

--- aoc_utils.py
+++ aoc_utils.py
@@ -1,6 +1,10 @@
-def read_txt_file_lines(filename='input.txt'):
-    with open(filename, 'r') as f:
+def read_txt_file_lines(filename='input.txt', read_arg='r'):
+    with open(filename, read_arg) as f:
         return [line.strip() for line in f.readlines()]
+    
+def read_txt_file_without_escape(filename='input.txt', read_arg='r'):
+    with open(filename, read_arg) as f:
+        return f.read().strip()
     
 def gcd(a, b):
     """find common divisor of all diffs"""   
